d855afedb5f0cd6ec63d80cfb076620314ce1a38
leassis91 <leassis.destefani@gmail.com>
2025-04-12 19:26:52 -0300

    atualiza footer page

--- src/assets/configs/footerConfig.js
+++ src/assets/configs/footerConfig.js
@@ -1,5 +1,6 @@
 import {AiFillGithub} from "react-icons/ai";
 import {FaLinkedinIn} from "react-icons/fa";
+import {FaKaggle} from "react-icons/fa";
 import {BsMedium} from "react-icons/bs";
 import React from "react";
 
@@ -7,21 +8,28 @@ const footerConfig = {
     icons: [
         {
             id: "footer-0",
-            url: "https://github.com/rodrigo-arenas",
+            url: "https://github.com/leassis91",
             className: "social-icon",
             target: "_blank",
             icon: <AiFillGithub size={50}/>
         },
         {
             id: "footer-1",
-            url: "https://linkedin.com/in/rodrigo-arenas-gomez",
+            url: "https://linkedin.com/in/leandrodestefani",
             className: "social-icon",
             target: "_blank",
             icon: <FaLinkedinIn size={50}/>
         },
         {
+            id: "footer-3",
+            url: "https://kaggle.com/leandrodestefani",
+            className: "social-icon",
+            target: "_blank",
+            icon: <FaKaggle size={50}/>
+        },
+        {
             id: "footer-2",
-            url: "https://rodrigo-arenas.medium.com",
+            url: "https://medium.com/@leandrodestefani",
             className: "social-icon",
             target: "_blank",
             icon: <BsMedium size={50}/>
