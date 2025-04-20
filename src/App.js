782a48191979a7fd8ac5fb3be56ff57eff7a1f30
leassis91 <leassis.destefani@gmail.com>
2025-04-20 18:48:11 -0300

    update app js

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
                 <Container sx={{ flex: 1, py: 3 }}>
                     <Analytics />
                     <Routes>
-                        <Route path="/portfolio" exact element={<Home />} />
+                        <Route path="/" exact element={<Home />} />
                         <Route path="/projects" exact element={<Projects />} />
                         <Route path="/blogs" exact element={<Blogs />} />
                         <Route path="/skills" exact element={<Skills />} />
